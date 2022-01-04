1fe07b84baaa3b826d2af88fad289747560bd005
jeff <jeffersonparil@gmail.com>
2022-01-04 17:43:27 +1100

    setting up CI

--- test/test.sh
+++ test/test.sh
@@ -2,9 +2,7 @@
 echo "##################################"
 echo "Test"
 echo "##################################"
-git clone https://github.com/jeffersonfparil/SeedMatic.git
-cd SeedMatic/src
-time python seed_dimensions.py \
+time python src/seed_dimensions.py \
                 -i ../res \
                 -e jpeg \
                 -W 20 \
